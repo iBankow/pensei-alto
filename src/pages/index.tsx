2c40952aa0accb61b414128b4cd835d0c39fe382
Tiago Bankow <bkw.designer@gmail.com>
2021-08-25 18:07:17 -0400

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { GlitchComponent } from "../components/GlitchComponent";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -7,9 +8,17 @@ export default function Home() {
       <Head>
         <title>Pensei Alto | Home</title>
       </Head>
-      <main>
-        <section>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
           <GlitchComponent text="pensei alto" />
+        </section>
+        <section className="text">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequatur sunt dignissimos, nobis officia blanditiis voluptate ad,
+            optio nemo totam praesentium quam quasi, magnam laboriosam similique
+            molestias. Perspiciatis dolor ex rem?
+          </p>
         </section>
       </main>
     </>
