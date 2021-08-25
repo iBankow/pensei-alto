f288a4bb95570c961db25cac79f116d1112cdd06
iBankow <bkw.designer@gmail.com>
2021-08-24 23:10:41 -0400

    initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>
+          Pensei Alto | Home
+        </title>
+      </Head>
+      <h1>Hello <span>World</span></h1>
+    </>
+  )
+}
